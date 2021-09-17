532f83e5d83cc9476fbcb316fac35720a914e3e2
GabrielGM01 <gabrielgusmaomoreno@gmail.com>
2021-09-17 20:35:26 -0300

    Subindo aula 5

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 // Dados
-const proffys = [
+/*const proffys = [
     {
     name:'Diego Fernands',avatar:'https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4'
     ,whatsapp:'61986739405',bio:'Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.'
@@ -18,52 +18,15 @@ const proffys = [
     ,time_from:[720]
     ,time_to:[1220]
     }
-];
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-]
-const weekdays = [
-    'Domingo',
-    'Segunda-feira',
-    'Terça-feira',
-    'Quarta-feira',
-    'Quinta-feira',
-    'Sexta-feira',
-    'Sábado',
-]
-// Funcionalidades
+];*/
 
-function getSubject(subjectNumber){
-    const position = +subjectNumber - 1;
-    return subjects[position];
-}
-function pageLading(req,res){
-    return res.render('index.html');
-}
-function pageStudy(req,res){
-    const filters = req.query;
-    return res.render('study.html',{proffys,filters,subjects,weekdays});
-}
-function pageGiveClasses(req,res){
-    const data = req.query;
-    // adicionar dados a lista de proffys
-    const isNotEmpty = Object.keys(data).length > 0;
-    if(isNotEmpty){
-        data.subject = getSubject(data.subject);
-        proffys.push(data);
-        return res.redirect("/study");
-    }
-    return res.render('give-classes.html',{subjects, weekdays});
-}
+const {
+    pageLading,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages');
+
 // Servidor
 const express = require('express');
 const server = express();
@@ -75,11 +38,16 @@ nunjucks.configure('src/views',{
     noCache: true
 });
 
+
+server
+// receber os dados do req.body
+.use(express.urlencoded({ extended:true}))
 // configurar arquivos estáticos 
-server.use(express.static('public'))
+.use(express.static('public'))
 // rotas da aplicação
 .get('/', pageLading)
 .get('/study', pageStudy)
 .get('/give-classes', pageGiveClasses)
+.post("/save-classes", saveClasses)
 // Start do servidor
 .listen(5000);
